13ad285fbd280f07889fa0ebd88a5969df5a2a07
Sukhil Hariharan <69431476+KANE10123@users.noreply.github.com>
2025-06-02 13:02:00 +0530

    Update settings.py

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -15,7 +15,7 @@ from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-ANGULAR_DIR = BASE_DIR.parent / "frontend/dist/frontend/browser"
+#ANGULAR_DIR = BASE_DIR.parent / "frontend/dist/frontend/browser"
 
 
 # Quick-start development settings - unsuitable for production
@@ -27,9 +27,9 @@ SECRET_KEY = (
 )
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["localhost", "127.0.0.1"]
 
 # Authentication
 AUTH_USER_MODEL = "users.User"
@@ -70,7 +70,7 @@ ROOT_URLCONF = "config.urls"
 TEMPLATES = [
     {
         "BACKEND": "django.template.backends.django.DjangoTemplates",
-        "DIRS": [BASE_DIR / "templates", ANGULAR_DIR],
+        "DIRS": [BASE_DIR / "templates"],
         "APP_DIRS": True,
         "OPTIONS": {
             "context_processors": [
@@ -86,8 +86,8 @@ TEMPLATES = [
 WSGI_APPLICATION = "config.wsgi.application"
 
 CORS_ALLOW_ALL_ORIGINS = True
-CORS_URLS_REGEX = r"^/api/.*$"
-
+#CORS_URLS_REGEX = r"^/api/.*$"
+ CORS_ALLOWED_ORIGINS = ["localhost", "127.0.0.1"]
 
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
@@ -139,7 +139,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 STATIC_URL = "static/"
-STATICFILES_DIRS = [ANGULAR_DIR]
+#STATICFILES_DIRS = [ANGULAR_DIR]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
